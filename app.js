aaf5deaf65493cb0ecb455ed6a5f76aacd901148
filipwicha <5021567@gmail.com>
2019-10-14 21:30:38 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const mysqlConnection = require('./connection')
+const UsersRoutes = require('./routes/users')
+
+var app = express()
+app.use(bodyParser.json())
+
+app.use('/users', UsersRoutes)
+
+app.listen(3000)
---
+++ connection.js
@@ -0,0 +1,15 @@
+const mysql = require('mysql')
+
+var mysqlConnection = mysql.createConnection({
+    host: 'den1.mysql3.gear.host',
+    user: 'noowe',
+    password: 'noowe!',
+    database: 'noowe',
+    multipleStatements: true
+})
+
+mysqlConnection.connect((err) => {
+    console.log(!err ? "connected" : "Failed to connect")
+})
+
+module.exports = mysqlConnection
