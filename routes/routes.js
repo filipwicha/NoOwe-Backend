63b5dd04206cd75efb1cc0bdb4021d71d1298fcd
filipwicha <5021567@gmail.com>
2019-11-06 21:23:15 +0100

    checked JWT validation

--- routes/routes.js
+++ routes/routes.js
@@ -5,5 +5,5 @@ module.exports = function (app) {
   app.post('/auth/signup', [middleware.checkDuplicateUserNameOrEmail], userController.signup)
   app.post('/auth/signin', userController.signin)
 
-  app.get('/user', /* [middleware.verifyToken], */ userController.getUsers)
+  app.get('/user', [middleware.verifyToken], userController.getUsers)
 }
