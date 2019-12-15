f92e5712e3b7cc4723ecf33c7dec96626ba0c123
filipwicha <33604199+filipwicha@users.noreply.github.com>
2019-12-15 13:25:10 +0100

    s

--- controllers/budgetController.js
+++ controllers/budgetController.js
@@ -50,7 +50,7 @@ exports.create = (req, res) => {
             console.log("Error -> error creating budgetmember for owner " + err)
         })
 
-        var nicknames = JSON.parse(req.body.budget_members).filter(nickname => {return (nickname != "" || nickname != null ? true : false )})
+        var nicknames = req.body.budget_members.filter(nickname => {return (nickname != "" || nickname != null ? true : false )})
 
         nicknames.forEach(nickname => {
             BudgetMember.create({
