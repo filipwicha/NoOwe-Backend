18e6469b6956017805d1ba8973bb80c6e81ccc1f
filipwicha <33604199+filipwicha@users.noreply.github.com>
2019-12-19 21:32:40 +0100

    added emoji to category model

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const db = require('./config/db.config')
 var server
 
 // force: true will drop the table if it already exists
-db.sequelize.sync({ force: false }).then(() => {
+db.sequelize.sync({ force: true }).then(() => {
     console.log('\nRESYNC DONE\n')
     server = app.listen(process.env.PORT || 3000, function () {
         console.log('App listening at http://localhost:' + server.address().port)
